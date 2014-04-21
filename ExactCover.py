b8af0597fef275ee4b816487cbfea967e6c7bcb1
Don Huynh <donhuynh@g.harvard.edu>
2014-04-21 13:30:12 -0400

    change to .py

---
+++ ExactCover.py
@@ -0,0 +1,30 @@
+
+
+class Node(object):
+    def __init__(self):
+        self.data = 0
+        self.left = None
+        self.right = None
+        self.up = None
+        self.down = None
+        self.column = None
+
+class Header(object):
+        self.size = 0
+        self.left = None
+        self.right = None
+        self.up = None
+        self.down = None
+
+
+class DancingLink():
+    def __init__(self, matrix):
+        self.header_list = []
+        self.matrix = []
+        self.root = Node()
+
+
+
+
+
+
